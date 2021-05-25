4463ff48570a77006f432b71a237073a5fc95120
Lincoln Martinez <lincoln.martinez@tecsup.edu.pe>
2021-05-25 00:00:23 -0500

    Agregar archivos de proyecto.

---
+++ lab12MVVM/lab12MVVM/Views/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace lab12MVVM
+{
+    public partial class MainPage : ContentPage
+    {
+		int currentState = 1;
+		string mathOperator;
+		double firstNumber, secondNumber;
+
+		public MainPage()
+        {
+            InitializeComponent();
+			this.BindingContext = new ViewModels.ViewModelCalculator();
+		}
+
+	}
+}
